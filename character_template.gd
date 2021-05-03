7d0388e8fdcbae3bf2312eb6a654224e70c2fe57
BRISTLEN0SE <82426304+BRISTLEN0SE@users.noreply.github.com>
2021-05-03 12:23:46 +1200

    3/5/21
    worked on character template

--- character_template.gd
+++ character_template.gd
@@ -1,8 +1,22 @@
 extends Node
 
 var health = 100
-var speed = 100
+var speed = 0
 var star = 1
+var charname = "spiderman"
+var charnum = 1
 
-func _ready():
-	pass 
+
+
+func _physics_process(delta):
+	if GlobalVariables.slot1 == charnum or GlobalVariables.slot2 == charnum:
+	#timer
+		if GlobalVariables.full != true:
+			print(speed)
+			speed += 1
+		if speed > 1000:
+			GlobalVariables.full = true
+		if Input.is_action_just_pressed("ui_down"):
+			speed = 0
+			GlobalVariables.full = false
+		
--- global_variables.gd
+++ global_variables.gd
@@ -1,16 +1,12 @@
 extends Node
 
+#slot
+var slot1 = 1
+var slot2 = 2
+var slot3 = 3
+var slot4 = 4
+var slot5 = 5
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#turns
+var full = false
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
