d68a6f1b0ef47f43cbf189acaeb590119a3cb1c0
BRISTLEN0SE <82426304+BRISTLEN0SE@users.noreply.github.com>
2021-07-08 10:54:13 +1200

    8/7/21
    working on simplifing code

---
+++ test.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var health = 1
+var speed = 1
+var attack = 1
+
+func _physics_process(delta):
+	$characters/slot1.set_frame(0)
+	$characters/slot2.set_frame(1)
+	$characters/lot3.set_frame(2)
+	$characters/slot4.set_frame(3)
+	$characters/slot5.set_frame(4)
+	$characters/slot6.set_frame(5)
+	$characters/slot7.set_frame(6)
+	$characters/slot8.set_frame(7)
+	$characters/slot9.set_frame(8)
+	$characters/slot10.set_frame(9)
+	
