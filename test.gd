4d326cedf38d2048f6887fb66085fac74f5a8a7e
BRISTLEN0SE <82426304+BRISTLEN0SE@users.noreply.github.com>
2021-08-02 12:25:08 +1200

    2/8/21
    new system

--- test.gd
+++ test.gd
@@ -1,11 +1,23 @@
 extends Node
 
-var health = 1
+var health = 0
 var speed = 1
 var random = RandomNumberGenerator.new()
-var attack = 1
-var current = 0
 var magechosen = false
+
+var chara1health = 0
+var chara2health = 0
+var chara3health = 0
+var chara4health = 0
+var chara5health = 0
+var chara6health = 0
+var chara7health = 0
+var chara8health = 0
+var chara9health = 0
+var chara10health = 0
+var chara11health = 0
+
+
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_up"):
@@ -26,18 +38,25 @@ func _physics_process(delta):
 	match speed:
 		1: 
 			$attacks/attack1.set_frame(6-1)
+			health = chara6health
 		2:
 			$attacks/attack1.set_frame(4-1)
+			health = chara4health
 		3:
 			$attacks/attack1.set_frame(10-1)
+			health = chara10health
 		4:
 			$attacks/attack1.set_frame(1-1)
+			health = chara1health
 		5:
 			$attacks/attack1.set_frame(7-1)
+			health = chara7health
 		6:
 			$attacks/attack1.set_frame(8-1)
+			health = chara8health
 		7:
 			$attacks/attack1.set_frame(3-1)
+			health = chara3health
 		8:
 			random.randomize()
 			$attacks/attack1.hide()
@@ -45,15 +64,19 @@ func _physics_process(delta):
 			if magechosen == false:
 				$attacks/attack1mage.set_frame(random.randi_range(0,3))
 				magechosen = true
+			health = chara5health
 		9:
 			$attacks/attack1.show()
 			$attacks/attack1mage.hide()
 			$attacks/attack1.set_frame(9-1)
 			magechosen = false
+			health = chara9health
 		10:
 			$attacks/attack1.set_frame(2-1)
+			health = chara2health
 		11:
 			$attacks/attack1.set_frame(11-1)
+			health = chara11health
 	if speed > 11:
 		speed = 1
 		
