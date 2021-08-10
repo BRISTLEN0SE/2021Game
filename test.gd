b0d3ec973afcc1a530fd2556d1455fb35e97247c
BRISTLEN0SE <82426304+BRISTLEN0SE@users.noreply.github.com>
2021-08-10 12:25:19 +1200

    10/8/21
    working on new system attacks and health

--- test.gd
+++ test.gd
@@ -1,6 +1,8 @@
 extends Node
 
-var health = 0
+var current_health = 0
+var targeted_health = 0 
+
 var speed = 1
 var random = RandomNumberGenerator.new()
 var magechosen = false
@@ -21,17 +23,16 @@ var chara9 = player_char.count(9)
 var chara10 = player_char.count(10)
 var chara11 = player_char.count(11)
 
-var chara1health = 0
-var chara2health = 0
-var chara3health = 0
-var chara4health = 0
-var chara5health = 0
-var chara6health = 0
-var chara7health = 0
-var chara8health = 0
-var chara9health = 0
-var chara10health = 0
-var chara11health = 0
+var slot1health = 0
+var slot2health = 0
+var slot3health = 0
+var slot4health = 0
+var slot5health = 0
+var enemyslot1health = 0
+var enemyslot2health = 0
+var enemyslot3health = 0
+var enemyslot4health = 0
+var enemyslot5health = 0
 
 
 
@@ -75,49 +76,42 @@ func _physics_process(delta):
 			if chara6 > 0:
 				$attacks/attack1.set_frame(6-1)
 				$Selected/current.set_frame(6-1)
-				health = chara6health
 			else:
 				speed += 1
 		2:
 			if chara4 > 0:
 				$attacks/attack1.set_frame(4-1)
 				$Selected/current.set_frame(4-1)
-				health = chara4health
 			else:
 				speed += 1
 		3:
 			if chara10 > 0:
 				$attacks/attack1.set_frame(10-1)
 				$Selected/current.set_frame(10-1)
-				health = chara10health
 			else:
 				speed += 1
 		4:
 			if chara1 > 0:
 				$attacks/attack1.set_frame(1-1)
 				$Selected/current.set_frame(1-1)
-				health = chara1health
 			else:
 				speed += 1
 		5:
 			if chara7 > 0:
 				$attacks/attack1.set_frame(7-1)
 				$Selected/current.set_frame(7-1)
-				health = chara7health
 			else:
 				speed += 1
 		6:
 			if chara8 > 0:
 				$attacks/attack1.set_frame(8-1)
 				$Selected/current.set_frame(8-1)
-				health = chara8health
 			else:
 				speed += 1
 		7:
 			if chara3 > 0:
 				$attacks/attack1.set_frame(3-1)
 				$Selected/current.set_frame(3-1)
-				health = chara3health
 			else:
 				speed += 1
 		8:
@@ -129,54 +123,53 @@ func _physics_process(delta):
 				if magechosen == false:
 					$attacks/attack1mage.set_frame(random.randi_range(0,3))
 					magechosen = true
-				health = chara5health
 			else:
 				speed += 1
 		9:
 			if chara9 > 0:
 				$attacks/attack1.set_frame(9-1)
 				$Selected/current.set_frame(9-1)
-				health = chara9health
 			else:
 				speed += 1
 		10:
 			if chara2 > 0:
 				$attacks/attack1.set_frame(2-1)
 				$Selected/current.set_frame(2-1)
-				health = chara2health
 			else:
 				speed += 1
 		11:
 			if chara11 > 0:
 				$attacks/attack1.set_frame(11-1)
 				$Selected/current.set_frame(11-1)
-				health = chara11health
 			else:
 				speed += 1
-	
-	#print(player_char)
 
 
 
 
 func _on_enemy1_pressed():
 	targeted = GlobalVariables.enemy_slot1-1
+	targeted_health = enemyslot1health
 
 
 func _on_enemy2_pressed():
 	targeted = GlobalVariables.enemy_slot2-1
+	targeted_health = enemyslot2health
 
 
 func _on_enemy3_pressed():
 	targeted = GlobalVariables.enemy_slot3-1
+	targeted_health = enemyslot3health
 
 
 func _on_enemy4_pressed():
 	targeted = GlobalVariables.enemy_slot4-1
+	targeted_health = enemyslot4health
 
 
 func _on_enemy5_pressed():
 	targeted = GlobalVariables.enemy_slot5-1
+	targeted_health = enemyslot5health
 
 
 func _on_attack_1_pressed():
