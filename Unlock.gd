b2aed3bd122fbfda18e894f1655f4104b8620d45
BRISTLEN0SE <82426304+BRISTLEN0SE@users.noreply.github.com>
2021-09-17 09:57:05 +1200

    17/9/21
    unlock system

--- Unlock.gd
+++ Unlock.gd
@@ -2,9 +2,16 @@ extends Node
 #hi
 
 func _physics_process(delta):
-	if GlobalVariables.gold > 100:
-		pass
+	$gold.text = str(GlobalVariables.gold)
+	if Input.is_action_pressed("ui_down"):
+		GlobalVariables.gold += 1
 
 func _on_Back_pressed():
 	get_tree().change_scene("res://MainMenu.tscn")
 	
+
+
+
+func _on_Buy_pressed():
+	if GlobalVariables.gold >= 100:
+		GlobalVariables.gold -= 100
