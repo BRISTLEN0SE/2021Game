bc300fe31217819ff8899bc8d6b48bd27a5fd57d
BRISTLEN0SE <82426304+BRISTLEN0SE@users.noreply.github.com>
2021-11-11 14:59:07 +1300

    11/11/21
    unlock random

--- Unlock.gd
+++ Unlock.gd
@@ -1,14 +1,14 @@
 extends Node
 #hi
-var unlock = 0
+var crystal = [0,1,2,3,4,5,6]
+var chosen = 0
 
 func _physics_process(delta):
+	randomize()
 	$gold.text = str(GlobalVariables.gold)
 	if Input.is_action_pressed("ui_down"):
 		GlobalVariables.gold += 1
-	if unlock > 5:
-		unlock = 1
-	print(unlock)
+
 
 func _on_Back_pressed():
 	get_tree().change_scene("res://MainMenu.tscn")
@@ -19,9 +19,6 @@ func _on_Back_pressed():
 func _on_Buy_pressed():
 	if GlobalVariables.gold >= 100:
 		GlobalVariables.gold -= 100
-		unlock += 1
-		$crystal1.set_frame(unlock)
-		$crystal2.set_frame(unlock + 1)
-		$crystal3.set_frame(unlock + 2)
-
-		
+		chosen = crystal[randi() % crystal.size()]
+		$crystalfinal.set_frame(chosen)
+		print(chosen)
